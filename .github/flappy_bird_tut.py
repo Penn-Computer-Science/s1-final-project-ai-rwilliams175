e27913d840bcfba2db052668a39f7656130f5d01
rwilliams175 <rwilliams175@phm.k12.in.us>
2025-12-16 09:48:07 -0500

    finalized config

--- .github/flappy_bird_tut.py
+++ .github/flappy_bird_tut.py
@@ -393,9 +393,9 @@ def eval_genomes(genomes, config):
         draw_window(WIN, birds, pipes, base, score, gen, pipe_ind)
 
         # break if score gets large enough
-        '''if score > 20:
+        if score > 500:
             pickle.dump(nets[0],open("best.pickle", "wb"))
-            break'''
+            break
 
 
 def run(config_file):
